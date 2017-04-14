78e63fcab9aed5b0ef3fc8b018a0222987f46de1
freer821 <freergzy8212@hotmail.com>
2017-04-14 09:35:03 +0200

    add upload file

--- app/services/usermanager.js
+++ app/services/usermanager.js
@@ -2,8 +2,10 @@
  * Created by Zhenyu on 11.04.2017.
  */
 'use strict'
+const mkdirp = require('mkdirp');
 const logger = require('../common/logger');
 const db = require('../common/database');
+const config = require('../common/config');
 
 
 const handle = function (req, res, next) {
@@ -18,18 +20,31 @@ const adduser = function (req, res, next) {
     db.findUser(new_user, function (err, user) {
         if (err) {
             logger.error('error to call signup', err.message);
-            return;
         }
         if (user) {
             req.flash('message', 'email is aready used, pls try other email!');
         } else {
+            if (req.files) {
+                let iconfile = req.files.icon;
+                mkdirp(config.upload.icon+'/'+new_user.email, function(err) {
+                    if (err) {
+                        logger.error('failed to create folder', err.message);
+                        return;
+                    }
+                    iconfile.mv(config.upload.icon+'/'+new_user.email+'/'+iconfile.name, function (err) {
+                        if (err) {
+                            logger.error('failed to save icon ', err.message);
+                            return;
+                        }
+                        new_user.icon = '/upload'+'/'+new_user.email+'/'+iconfile.name;
+                    });
+                });
+            }
             db.saveUser(new_user);
-            res.render('login.html');
+            req.flash('message', 'user saved!');
         }
-
-    })
-
+        res.render('useroverview');
+    });
 };
-
 module.exports.handle = handle;
 module.exports.adduser = adduser;
