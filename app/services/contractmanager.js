6689253b258bcefaac4ce555d68ed2bfa8f35b84
ctsknight <ctsknight@gmail.com>
2017-05-04 16:13:54 +0800

    modify get contract id

--- app/services/contractmanager.js
+++ app/services/contractmanager.js
@@ -11,57 +11,53 @@ const addContract = function (req, res, next) {
         res.render('addandeditcontract', {title:'Project Management', project_id: req.query.project_id, project_adr:req.query.project_adr,  user: req.user});
     } else {
         let request = req.body;
+        db.countContract(function (err, count) {
+	        if (err) {
+	            logger.error('error to count contract');
+	            count = 0;
+	        }
+	        let contract = {
+	            id: getContractID(count),
+	            project_id: request.project_id,
+	            customer: request.customer,
+	            cost_code: request.cost_code,          // kst
+	            project_nr: request.project_nr,  // vom auftraggeber
+	            sap_nr: request.sap_nr,
+	            electric_nr: request.electric_nr,
+	            gas_nr: request.gas_nr,
+	            water_nr: request.water_nr,
+	            partner_name: request.partner_name,
+	            contract_delivery: request.contract_delivery? moment(request.contract_delivery): undefined,  // contract delivery auftrag_uebergeb
+	            doc_delivery: request.doc_delivery? moment(request.doc_delivery): undefined, // documents delivery unterlage_uebergeb
+	            work_content: request.work_content, // arbeitsbemerkung
+	            contract_typ: {            // auftrag typ
+	                electric: request.is_electric,     // elektro
+	                water: request.is_water,      // wasser
+	                gas: request.is_gas,         // gas
+	                telecom: request.is_telecom,     // telekom
+	                light: request.is_light,
+	                others: request.is_others        // others
+	            },
+	            isBombExisted: request.isBombExisted,    // bomb, weapon kampfmittell
+	            estimated_value: request.estimated_value,      // schaetzwert (Euro)
+	            manager_name: request.manager_name,   // bauleiter_name
+	            rot_b: request.rot_b,         // Auftrageber Telekom
+	            comment: request.comment,
+	            doc_location: {
+	                person: request.person,
+	                reason: request.reason
+	            }
+	        };
 
-        let contract = {
-            id: getContractID(),
-            project_id: request.project_id,
-            customer: request.customer,
-            cost_code: request.cost_code,          // kst
-            project_nr: request.project_nr,  // vom auftraggeber
-            sap_nr: request.sap_nr,
-            electric_nr: request.electric_nr,
-            gas_nr: request.gas_nr,
-            water_nr: request.water_nr,
-            partner_name: request.partner_name,
-            contract_delivery: request.contract_delivery? moment(request.contract_delivery): undefined,  // contract delivery auftrag_uebergeb
-            doc_delivery: request.doc_delivery? moment(request.doc_delivery): undefined, // documents delivery unterlage_uebergeb
-            work_content: request.work_content, // arbeitsbemerkung
-            contract_typ: {            // auftrag typ
-                electric: request.is_electric,     // elektro
-                water: request.is_water,      // wasser
-                gas: request.is_gas,         // gas
-                telecom: request.is_telecom,     // telekom
-                light: request.is_light,
-                others: request.is_others        // others
-            },
-            isBombExisted: request.isBombExisted,    // bomb, weapon kampfmittell
-            estimated_value: request.estimated_value,      // schaetzwert (Euro)
-            manager_name: request.manager_name,   // bauleiter_name
-            rot_b: request.rot_b,         // Auftrageber Telekom
-            comment: request.comment,
-            doc_location: {
-                person: request.person,
-                reason: request.reason
-            }
-        };
-
-        db.editContract({id: contract.id},contract);
-        res.redirect('/projects');
+	        db.editContract({id: contract.id},contract);
+	        res.redirect('/projects');	        
+    	});        	
     }
 };
 
-function getContractID() {
+function getContractID(count) {
     // TODO
-    db.countContract(function (err, count) {
-        if (err) {
-            logger.error('error to count contract');
-            return '17-44-0001-01';
-        } else {
-        	let id = '17-44-0001-' + (count+1).toString();
-        	logger.error('generate contractid' + id);
-        	return id;
-        }
-    });
+    return '17-44-0001-' + (count+1).toString();
 }
 
 const getContractByProjectID = function (req, res, next) {
