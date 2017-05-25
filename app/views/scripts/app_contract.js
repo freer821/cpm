4580e9f7215b459d5cb907dfad0b33780f916998
freer821 <freergzy8212@hotmail.com>
2017-05-25 17:36:35 +0200

    create contract-basic

--- app/views/scripts/app_contract.js
+++ app/views/scripts/app_contract.js
@@ -1,3 +1,18 @@
 /**
  * Created by Zhenyu on 25.05.2017.
  */
+(function ($) {
+    'use strict';
+    $('#contract-basic-modal').on('show.bs.modal', function(e) {
+        $(this).find('form')[0].reset();
+        $("#project_id").val($(e.relatedTarget).data('project_id'));
+        var contract = $(e.relatedTarget).data('project');
+        if(contract){
+            //set title
+        } else {
+            //set title
+
+        }
+    });
+
+})(jQuery);
