35303f500403e84e1bdebc026ee8e9b939486312
freer821 <freergzy8212@hotmail.com>
2017-06-18 20:41:34 +0200

    bugfixing

--- app/views/scripts/app/app_project.js
+++ app/views/scripts/app/app_project.js
@@ -38,11 +38,6 @@ $(document).ready(function(){
         }
     });
 
-    $('#projects-overview tbody').on('click', 'td.details-control', function (e) {
-        var row = $(this).closest('tr');
-
-    });
-
 });
 
 function showContracts(project_id) {
@@ -68,22 +63,16 @@ function hideContracts(project_id) {
     $('#'+project_id+'_contracts').parent().remove();
 }
 
-function displayContractDetail(contract_id) {
-    $('#'+contract_id).removeClass("closed_contract");
-    //$('#'+contract_id).addClass("opened_contract");
-
-}
-
 function contractOverview (contract) {
     // generate contract overview
-        return formatContractStatus(contract) +
-                        '<td class="contractSchema.id" style="width:150px;min-width:150px;max-width:150px;">H00002-17-44-0001 </td>'+
-                        '<td class="contractSchema.street&amp;contractSchema.housenr" style="min-width:150px;">Muster str. 1</td>'+
-                        '<td class="contractSchema.work_content" style="min-width:150px;">all typen</td>'+
-                        '<td class="contractSchema.cost_code" style="min-width:80px;">734044 </td>'+
-                        '<td class="contractSchema.customer" style="min-width:70px;">STW </td>'+
-                        '<td class="contractSchema.contract_id" style="min-width:110px;">450003971 </td>'+
-                        '<td class="contract_edit" style="min-width:45px;max-width:45px;"><i class="fa fa-pencil-square-o" style="font-size:22px;margin-left:4px;color:rgba(131,175,155,0.8);min-width:22px;"></i></td>'+
+     return formatContractStatus(contract) +
+                        '<td class="contractSchema.id" style="width:150px;min-width:150px;max-width:150px;">'+contract.id+' </td>'+
+                        '<td class="contractSchema.street&amp;contractSchema.housenr" style="min-width:150px;">'+contract.contract_street+'</td>'+
+                        '<td class="contractSchema.work_content" style="min-width:150px;">'+contract.work_content+'</td>'+
+                        '<td class="contractSchema.cost_code" style="min-width:80px;">'+contract.cost_code+' </td>'+
+                        '<td class="contractSchema.customer" style="min-width:70px;">'+contract.customer+' </td>'+
+                        '<td class="contractSchema.contract_id" style="min-width:110px;">'+contract.contract_id+' </td>'+
+                        '<td class="contract_edit" style="min-width:45px;max-width:45px;"><a href="#" data-toggle="modal" data-target="#contract-basic-modal" data-contract=\''+JSON.stringify(contract)+'\' data-backdrop="static"><i class="fa fa-pencil-square-o" style="font-size:22px;margin-left:4px;color:rgba(131,175,155,0.8);min-width:22px;"></i></a></td>'+
                         '<td class="contract_types&amp;lock" style="height:42px;padding:0px;min-width:92px;width:92px;max-width:92px;">'+
                             '<div style="padding:0px;height:40px;">'+
                                 '<div style="height:42px;min-height:0px;font-size:0px;">'+
@@ -93,13 +82,9 @@ function contractOverview (contract) {
                                         '</thead>'+
                                         '<tbody style="height:42px;">'+ 
                                             '<tr style="height:15%;">'+
-                                                '<td rowspan="0" style="width:40%;padding-top:10%;padding-left:10%;padding-bottom:10%;padding-right:0%;height:42px;"><i class="fa fa-unlock-alt contract_lock_false" style="font-size:22px;margin-left:4px;color:rgba(131,175,155,0.8);min-width:22px;"></i></td>'+
-                                                '<td rowspan="0" class="projectSchema.contract_types.electric" style="width:5%;padding:0;background-color:#b53f45;">1</td>'+
-                                                '<td rowspan="0" class="projectSchema.contract_types.gas" style="width:5%;padding:0;background-color:#ffd000;">1</td>'+
-                                                '<td rowspan="0" class="projectSchema.contract_types.water" style="width:5%;padding:0;height:42px;background-color:#3e6b7e;">1</td>'+
-                                                '<td rowspan="0" class="projectSchema.contract_types.light" style="width:5%;padding:0;background-color:#91c0ab;">1</td>'+
-                                                '<td rowspan="0" class="projectSchema.contract_types.telecom" style="width:5%;padding:0;background-color:#f87e7b;">1</td>'+
-                                                '<td rowspan="0" class="projectSchema.contract_types.others" style="width:5%;padding:0;background-color:#333333;">1</td>'+
+                                                '<td rowspan="0" style="width:40%;padding-top:10%;padding-left:10%;padding-bottom:10%;padding-right:0%;height:42px;">'+
+                                                '<i class="fa fa-unlock-alt contract_lock_false" style="font-size:22px;margin-left:4px;color:rgba(131,175,155,0.8);min-width:22px;"></i></td>'+
+                                                formatContractType(contract)+
                                             '</tr>'+
                                         '</tbody>'+
                                     '</table>'+
@@ -109,22 +94,80 @@ function contractOverview (contract) {
                     '</tr>';
 }
 
+function openContractDetail(contract_id) {
+    $('#'+contract_id+'_details').show();
+    $('#'+contract_id+' i.fa-chevron-circle-right').hide();
+    $('#'+contract_id+' i.fa-chevron-circle-down').show();
+}
+
+function closeContractDetail(contract_id) {
+    $('#'+contract_id+'_details').hide();
+    $('#'+contract_id+' i.fa-chevron-circle-right').show();
+    $('#'+contract_id+' i.fa-chevron-circle-down').hide();
+}
+
+function formatContractType(contract) {
+    var contract_types ='';
+    if (contract.contract_typ.electric) {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.electric" style="width:5%;padding:0;background-color:#b53f45;">1</td>';
+    } else {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.electric" style="width:5%;padding:0;background-color:#ffffff;">1</td>'
+    }
+
+    if (contract.contract_typ.gas) {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.gas" style="width:5%;padding:0;background-color:#ffd000;">1</td>';
+    } else {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.gas" style="width:5%;padding:0;background-color:#ffffff;">1</td>'
+    }
+
+    if (contract.contract_typ.water) {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.water" style="width:5%;padding:0;height:42px;background-color:#3e6b7e;">1</td>';
+    } else {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.water" style="width:5%;padding:0;background-color:#ffffff;">1</td>'
+    }
+
+    if (contract.contract_typ.light) {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.light" style="width:5%;padding:0;background-color:#91c0ab;">1</td>';
+    } else {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.light" style="width:5%;padding:0;background-color:#ffffff;">1</td>'
+    }
+
+    if (contract.contract_typ.telecom) {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.telecom" style="width:5%;padding:0;background-color:#f87e7b;">1</td>';
+    } else {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.telecom" style="width:5%;padding:0;background-color:#ffffff;">1</td>'
+    }
+
+    if (contract.contract_typ.others) {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.others" style="width:5%;padding:0;background-color:#333333;">1</td>';
+    } else {
+        contract_types +=  '<td rowspan="0" class="contract.contract_typ.others" style="width:5%;padding:0;background-color:#ffffff;">1</td>'
+    }
+
+    return contract_types;
+}
+
+
 function formatContractStatus(contract){
     switch (contract.total_status) {
         case 'UNFINISHED':
-            return '<tr class="contract_not_opened"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;"><i onclick="displayContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i></td>'+
+            return '<tr id="'+contract.id+'" class="contract_not_opened"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;">'+
+                '<i onclick="openContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i><i onclick="closeContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-down" style="display:none;font-size:18px;"></i></td>'+
                 '<td class="2nd_unfold" style="margin-left:0px;min-width:32px;max-width:32px;width:32px;"><i class="fa fa-spinner" style="font-size:20px;color:rgb(131,175,155);"></i></td>';
             break;
         case 'FINISHED':
-            return '<tr class="contract_finished" style="color:rgba(131,175,155,0.8);"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;"><i onclick="displayContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i></td>'+
+            return '<tr id="'+contract.id+'" class="contract_finished" style="color:rgba(131,175,155,0.8);"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;">'+
+                '<i onclick="openContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i><i onclick="closeContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-down" style="display:none;font-size:18px;"></i></td>'+
                 '<td class="2nd_unfold" style="margin-left:0px;min-width:32px;max-width:32px;width:32px;"><i class="fa fa-check-square-o" style="font-size:20px;color:rgb(131,175,155);"></i></td>';
             break;
         case 'DEAKTIV':
-            return '<tr class="contract_deaktiv" style="color:rgb(182,182,182);"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;"><i onclick="displayContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i></td>'+
+            return '<tr id="'+contract.id+'" class="contract_deaktiv" style="color:rgb(182,182,182);"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;">'+
+                '<i onclick="openContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i><i onclick="closeContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-down" style="display:none;font-size:18px;"></i></td>'+
                 '<td class="2nd_unfold" style="margin-left:0px;min-width:32px;max-width:32px;width:32px;"><i class="fa fa-hand-stop-o" style="font-size:20px;color:rgb(131,175,155);"></i></td>';
             break;
         default:
-            return '<tr class="contract_not_opened"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;"><i onclick="displayContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i></td>'+
+            return '<tr id="'+contract.id+'" class="contract_not_opened"><td class="2nd_unfold" style="margin-left:0px;width:40px;min-width:40px;max-width:40px;">'+
+                '<i onclick="openContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-right" style="font-size:18px;"></i><i onclick="closeContractDetail(\''+contract.id+'\')" class="fa fa-chevron-circle-down" style="display:none;font-size:18px;"></i></td>'+
                 '<td class="2nd_unfold" style="margin-left:0px;min-width:32px;max-width:32px;width:32px;"><i class="fa fa-check-square-o" style="font-size:20px;color:rgb(131,175,155);"></i></td>';
             break;
     }
@@ -132,7 +175,7 @@ function formatContractStatus(contract){
 
 function contractDetail(contract) {
     // `d` is the original data object for the row
-    return '<div id="'+contract.id+'" class="closed_contract"><div class="row" style="padding-left:50px;padding-right:0px;">'+
+    return '<div id="'+contract.id+'_details" style="display:none"><div class="row" style="padding-left:50px;padding-right:0px;">'+
         '<div class="col-lg-6 col-md-6 col-sm-6">'+
         '<div class="row">'+
         '<div class="col-lg-2 col-md-2 col-sm-2" style="padding-left:26px;margin-top:15px;"><a href="#" data-toggle="modal" data-target="#contract-permissions-modal" data-contract=\''+JSON.stringify(contract)+'\' data-backdrop="static" class="list-left"><i class="fa fa-thumbs-o-up" style="font-size:30px;"></i></a></div>'+
@@ -317,7 +360,7 @@ function contractDetail(contract) {
         '<label>Comment</label>'+
         '</div>'+
         '<div class="row">'+
-        '<textarea class="contractSchema.comment" style="width:99%;">'+'</textarea>'+
+        '<textarea class="contractSchema.comment" style="width:99%;" '+contract.comment+'></textarea>'+
         '</div>'+
         '</div>'+
         '</div>'+
